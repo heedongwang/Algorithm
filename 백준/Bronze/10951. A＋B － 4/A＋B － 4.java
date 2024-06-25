de5124c45e386e485150a0497e6445f77c57b085
heedongwang <kim3he@gmail.com>
2024-06-25 22:52:40 +0900

    [Bronze V] Title: A+B - 4, Time: 236 ms, Memory: 17908 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args)  {
+		
+		
+		Scanner sc=new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			String s[]= sc.nextLine().split(" ");
+			System.out.println(Integer.parseInt(s[0])+Integer.parseInt(s[1]));
+		}
+		
+		
+		
+		
+		
+
+	}
+}
