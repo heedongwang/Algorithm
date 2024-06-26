3c3d7ec007380345b25e70fbb2416d6bca499a06
heedongwang <kim3he@gmail.com>
2024-06-26 09:12:19 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    
+    if(0<=num1<=10000 && 0<=num2<=10000){
+        if(num1==num2)
+            return 1;
+        else
+            return -1;
+            
+    }
+    
+}
