b032fa74847ec6c1bccb0a47155eefecb58522c1
heedongwang <kim3he@gmail.com>
2024-07-02 11:50:30 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    if n<8:
+        answer=1
+    else:
+        if n%7==0:
+            answer=(n//7)
+        else:
+            answer=(n//7)+1
+    
+    
+    return answer
