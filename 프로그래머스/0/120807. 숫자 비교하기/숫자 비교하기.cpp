47a48134123a995c2c399e7585761d67e410cd19
왕히동👑 <kim3he@gmail.com>
2024-08-21 11:57:16 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
@@ -4,13 +4,11 @@
 using namespace std;
 
 int solution(int num1, int num2) {
-    
-    if(0<=num1<=10000 && 0<=num2<=10000){
-        if(num1==num2)
-            return 1;
-        else
-            return -1;
-            
+    int answer = 0;
+    if(num1==num2){
+        answer=1;
+    }else{
+        answer=-1;
     }
-    
+    return answer;
 }
